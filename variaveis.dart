fb45b1d924e9b5a9c1c821f3f91c55415dd2fbeb
bia miranda <112702152+bia-miranda@users.noreply.github.com>
2023-09-03 08:14:43 -0300

    Create variaveis.dart

---
+++ variaveis.dart
@@ -0,0 +1,7 @@
+// 1 - variáveis
+  String nome = "Beatriz";
+  int idade = 18;
+  bool estuda = true;
+  
+  List<String> lista = ["$nome","$idade", "$estuda"];
+  print("${lista[0]} - ${lista[1]} - ${lista[2]}");
