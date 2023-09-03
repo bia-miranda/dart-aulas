f3111ca388b05d5fbcf034fbbdb3ff25ff4cf8fa
bia miranda <112702152+bia-miranda@users.noreply.github.com>
2023-09-03 09:55:26 -0300

    Create if-switch.dart

---
+++ if-switch.dart
@@ -0,0 +1,28 @@
+  // 3 - if e switch
+  
+  bool seguirFrente = true;
+
+  if(seguirFrente == true){
+    print("Andar");
+  }
+  else{
+    print("Parar");
+  }
+  
+  if(10 > 5){
+    print("10 é maior que 5");
+  }
+  
+  int mundialPalmeiras = 0;
+  switch(mundialPalmeiras){
+    case 1: 
+      print("Mentira");
+      break;
+    
+    case 2:
+      print("Mentira de novo!");
+      break;  
+      
+    default:
+      print("PALMEIRAS NÃO TEM MUNDIAL");
+  }
